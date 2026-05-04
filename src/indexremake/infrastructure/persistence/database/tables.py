82d3d2493c8183636559f279db28ad19535c21ea
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-05-04 12:04:59 +0200

    fix(infrastructure): specify non nullable columns

--- src/indexremake/infrastructure/persistence/database/tables.py
+++ src/indexremake/infrastructure/persistence/database/tables.py
@@ -5,27 +5,27 @@ from indexremake.infrastructure.persistence.database import base
 folders = sa.Table(
     "folders",
     base.metadata,
-    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
-    sa.Column("year", sa.Integer),
+    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True, nullable=False),
+    sa.Column("year", sa.Integer, nullable=False),
 )
 
 documents = sa.Table(
     "documents",
     base.metadata,
-    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
-    sa.Column("folder_id", sa.Integer, sa.ForeignKey("folders.id")),
-    sa.Column("document_number", sa.Integer),
-    sa.Column("title", sa.String),
+    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True, nullable=False),
+    sa.Column("folder_id", sa.Integer, sa.ForeignKey("folders.id"), nullable=False),
+    sa.Column("document_number", sa.Integer, nullable=False),
+    sa.Column("title", sa.String, nullable=False),
 )
 
 users = sa.Table(
     "users",
     base.metadata,
-    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
-    sa.Column("document_id", sa.Integer, sa.ForeignKey("documents.id")),
-    sa.Column("position", sa.Integer),
-    sa.Column("first_name", sa.String),
+    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True, nullable=False),
+    sa.Column("document_id", sa.Integer, sa.ForeignKey("documents.id"), nullable=False),
+    sa.Column("position", sa.Integer, nullable=False),
+    sa.Column("first_name", sa.String, nullable=False),
     sa.Column("middle_name", sa.String),
-    sa.Column("last_name1", sa.String),
+    sa.Column("last_name1", sa.String, nullable=False),
     sa.Column("last_name2", sa.String),
 )
