dcf7020e353b6619a2885c40e558f8705fe4bd47
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-05-07 12:20:12 +0200

    refactor(tests): use test data from support module

--- tests/integration/test_queries.py
+++ tests/integration/test_queries.py
@@ -1,49 +1,22 @@
 import pytest
-from sqlalchemy import orm
+import pytest_cases
 
 from indexremake import domain, dtos
-from indexremake.infrastructure.persistence import queries
+from indexremake.infrastructure.persistence import database, queries
+from tests.support import data, factories
 
 
 @pytest.fixture
-def users() -> list[domain.User]:
-    return [
-        domain.User("John", None, "Doe", "Tre"),
-        domain.User("Jane", None, "Doe", "Fire"),
-        domain.User("Juan", "Eriberto", "Five", "Six of Seven"),
-        domain.User("Camilo", None, "Cruz", "Pereira"),
-        domain.User("Amalia", "de la Caridad", "Rodríguez", "Díaz"),
-        domain.User("Marco", "Polo", "Traveller", None),
-    ]
+def seed_data() -> list[factories.FolderData]:
+    return data.load_test_data()
 
 
 @pytest.fixture
-def documents(users: list[domain.User]) -> list[domain.Document]:
-    return [
-        domain.Document(1, "Document 1", users[:2]),
-        domain.Document(2, "Document 2", [users[2]]),
-        domain.Document(3, "Document 3", users[3:]),
-    ]
+def folders(seed_data: list[factories.FolderData]) -> list[domain.Folder]:
+    return factories.build_folders(seed_data)
 
 
-@pytest.fixture
-def folders(documents: list[domain.Document]) -> list[domain.Folder]:
-    return [
-        domain.Folder(2025, documents[:2]),
-        domain.Folder(2026, [documents[2]]),
-        domain.Folder(2027, []),
-    ]
-
-
-@pytest.fixture
-def db_with_data(db_session: orm.Session, folders: list[domain.Folder]) -> orm.Session:
-    db_session.add_all(folders)
-    db_session.commit()
-    return db_session
-
-
-@pytest.fixture
-def document_summary_dtos(
+def build_summaries(
     documents: list[domain.Document],
 ) -> list[dtos.DocumentSummaryDTO]:
     return [
@@ -56,34 +29,32 @@ def document_summary_dtos(
             doc.users[0].last_name2,
             len(doc.users),
         )
-        for doc in documents[:2]
+        for doc in documents
     ]
 
 
-def test_query_can_return_only_documents_for_a_given_year(
-    db_with_data: orm.Session,
-    document_summary_dtos: list[dtos.DocumentSummaryDTO],
+def test_query_can_return_documents_for_a_given_year(
+    seeded_db: database.SQLiteDatabase,
+    folders: list[domain.Folder],
 ) -> None:
+    summaries = build_summaries(folders[0].documents)
+    session = seeded_db.get_session()
 
-    fetched_docs = queries.get_documents_per_year(db_with_data, 2025)
+    fetched_docs = queries.get_documents_per_year(session, 2025)
 
-    assert len(document_summary_dtos) == len(fetched_docs)
-    assert document_summary_dtos == fetched_docs
+    assert len(summaries) == len(fetched_docs)
+    assert summaries == fetched_docs
 
 
-def test_query_returns_empty_list_if_no_documents_for_a_given_year(
-    db_with_data: orm.Session,
+@pytest_cases.parametrize(
+    "year", [2027, 9999], ids=["No documents for year", "No year in database"]
+)
+def test_query_returns_empty_list(
+    seeded_db: database.SQLiteDatabase,
+    year: int,
 ) -> None:
+    session = seeded_db.get_session()
 
-    fetched_docs = queries.get_documents_per_year(db_with_data, 2027)
-
-    assert len(fetched_docs) == 0
-
-
-def test_query_returns_empty_list_for_an_unknown_year(
-    db_with_data: orm.Session,
-) -> None:
-
-    fetched_docs = queries.get_documents_per_year(db_with_data, 9999)
+    fetched_docs = queries.get_documents_per_year(session, year)
 
     assert len(fetched_docs) == 0
